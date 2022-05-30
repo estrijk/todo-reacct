993712bdaf2505fdedbbd561df27c7b6304a8a2e
Erik Strijk <edstrijk@st.noorderpoort.nl>
2022-05-30 11:27:28 +0200

    use effect

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,11 +1,12 @@
+import React, { useEffect } from "react";
+
 function ToDoItem(props) {
-    return (
-      <div>
-          <p> { props.title } </p>
-          <button onClick={() => props.removeTodo(props.index) }> remove </button>
-      </div>
-    );
-  }
-  
-  export default ToDoItem;
-  
+  return (
+    <div>
+      <p> {props.title} </p>
+      <button onClick={() => props.removeTodo(props.index)}> remove </button>
+    </div>
+  );
+}
+
+export default ToDoItem;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,32 +1,50 @@
-import ToDoInput from '../components/todo-input';
-import { useState } from "react";
-import '../styles/app.css';
-import ToDoItem from '../components/todo-item';
+import ToDoInput from "../components/todo-input";
+import { useState, useEffect } from "react";
+import "../styles/app.css";
+import ToDoItem from "../components/todo-item";
 
 function App() {
-    const [todos, setTodos] = useState([]);
-    const todoList = [];
+  const [todos, setTodos] = useState([]);
+  let todoList = [];
 
-    todos.forEach((todo, index) => {
-        todoList.push(<ToDoItem title={todo} removeTodo={removeTodo} index={index} key={index}/>);
-    });
-    function addTodo(todo) {
-        setTodos([...todos, todo]);
-    }
-    function removeTodo(todo) {
-        const tempTodos = [...todos];
-        tempTodos.splice(todo, 1);
+  todos.forEach((todo, index) => {
+    todoList.push(
+      <ToDoItem
+        title={todo}
+        removeTodo={removeTodo}
+        index={index}
+        key={index}
+      />
+    );
+  });
+  
+  function addTodo(todo) {
+    setTodos([...todos, todo]);
+  }
+  function removeTodo(todo) {
+    const tempTodos = [...todos];
+    tempTodos.splice(todo, 1);
 
-        setTodos(tempTodos);
-    }
-    return (
+    setTodos(tempTodos);
+  }
+
+  useEffect(() => {
+    let tempTodos = JSON.parse(localStorage.getItem("todos"));
+
+    setTodos(tempTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
     <div className="wrapper">
-        <header>
-            <h1> To Do List </h1>
-            <ToDoInput addTodo={addTodo}/>
-        </header>
-        {todoList.length > 0 && <div className='todos'> { todoList } </div> }
-        
+      <header>
+        <h1> To Do List </h1>
+        <ToDoInput addTodo={addTodo} />
+      </header>
+      {todoList.length > 0 && <div className="todos"> {todoList} </div>}
     </div>
   );
 }
